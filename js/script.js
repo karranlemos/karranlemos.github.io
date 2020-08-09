30be30fda58f6b80f85f48560253f7fc1b4eb86d
Karran Lemos <karranlemos.uni@gmail.com>
2020-08-09 15:50:45 -0300

    Mobile buttons now functional

--- js/script.js
+++ js/script.js
@@ -12,9 +12,26 @@ class MainMenu {
         this.menuOptions = this.navbar.querySelector('.menu-options')
         if (!this.menuOptions)
             throw 'Menu options not found'
+
+        this.homeButton = this.navbar.querySelector('.home-button')
+        if (this.homeButton) {
+            this.homeButton.addEventListener('click', this.goToTopOfPage)
+        }
+
+        this.subpagesButtons = this.menuOptions.getElementsByClassName('menu-button')
+        for (let subpagesButton of this.subpagesButtons) {
+            let sectionId = subpagesButton.getAttribute('data-section-id')
+            if (!sectionId)
+                continue
+            let section = document.getElementById(sectionId)
+            if (!section)
+                continue
+            subpagesButton.addEventListener('click', function() {
+                this.goToSection(section)
+            }.bind(this))
+        }
         
         this.mobileButton.addEventListener('click', this.toggleMobileMenu.bind(this))
-
         window.addEventListener('scroll', this.checkNavbarFixed.bind(this))
     }
 
@@ -28,6 +45,20 @@ class MainMenu {
             this.navbar.classList.add('pinned')
         else
             this.navbar.classList.remove('pinned')
+    }
+
+    goToTopOfPage() {
+        window.scrollTo(0, 0)
+    }
+
+    goToSection(section) {
+        const NAVBAR_HEIGHT_PINNED = 80
+        var sectionY = section.offsetTop
+        
+        var oldX = window.pageXOffset
+        var newY = sectionY-NAVBAR_HEIGHT_PINNED
+        
+        window.scrollTo(oldX, newY)
     }
 }
 
