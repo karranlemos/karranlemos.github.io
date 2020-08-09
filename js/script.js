0ce350c8cbb1c39f6224e42e8406848ceb46e2fe
Karran Lemos <karranlemos.uni@gmail.com>
2020-08-09 15:12:20 -0300

    Add mobile menu

--- js/script.js
+++ js/script.js
@@ -6,6 +6,14 @@ class MainMenu {
         this.navbar = document.getElementById('main-menu')
         if (!this.navbar)
             throw 'Main menu not found'
+        this.mobileButton = this.navbar.querySelector('.mobile-button')
+        if (!this.mobileButton)
+            throw 'Mobile button not found'
+        this.menuOptions = this.navbar.querySelector('.menu-options')
+        if (!this.menuOptions)
+            throw 'Menu options not found'
+        
+        this.mobileButton.addEventListener('click', this.toggleMobileMenu.bind(this))
 
         window.addEventListener('scroll', function() {
             if (window.pageYOffset > this.MIN_PINNED_NAVBAR)
@@ -14,7 +22,24 @@ class MainMenu {
                 this.navbar.classList.remove('pinned')
         }.bind(this))
     }
+
+    async toggleMobileMenu() {
+        this.navbar.classList.toggle('mobile-open')
+    }
 }
+
+
+
+class Helpers {
+    constructor() {
+        throw 'Static class'
+    }
+
+    static sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+}
+
 
 
 var scriptObjects = {}
